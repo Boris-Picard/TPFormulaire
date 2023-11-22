81892ee13f2d4ab809a06b0810ad10beb113846c
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 <picard.boris@gmail.com>
2023-11-22 16:45:26 +0100

    boucle a faire pays de naissance

--- index.php
+++ index.php
@@ -1,14 +1,16 @@
 <?php
-    $error = [];
+    define('LAST_NAME', '^[A-Za-zéèçà \-]{2,50}$');
+    define('POSTAL_CODE', '^[0-9]{5}$');
     if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $error = [];
         // LASTNAME
         $lastname = filter_input(INPUT_POST, 'lastname', FILTER_SANITIZE_SPECIAL_CHARS);
         if(empty($lastname)) {
             $error['lastname'] = 'Veuillez entrer un Nom';
         } else {
-            $isOk = filter_var($lastname, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>'/^[A-Za-zéèçà \-]{2,50}$/')));
+            $isOk = filter_var($lastname, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>'/'.LAST_NAME.'/')));
             if(!$isOk) {
-                $error['lastname'] = 'votre nom n\'est pas valide';
+                $error['lastname'] = 'Votre nom n\'est pas valide';
                 } 
             }
         // EMAIL
@@ -24,13 +26,35 @@
         // ZIP CODE
         $postalCode = filter_input(INPUT_POST, 'postalCode', FILTER_SANITIZE_NUMBER_INT);
         if(empty($postalCode)) {
-            $error['postalCode'] = 'Veuillez entrer un code postal';
+            $error['postalCode'] = 'Veuillez entrer un code postal valide';
         } else {
-            $isOk = filter_var($postalCode, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>'/^[0-9]{5}$/')));
+            $isOk = filter_var($postalCode, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>'/'.POSTAL_CODE.'/')));
             if(!$isOk) {
                 $error['postalCode'] = 'Votre code postal n\'est pas valide';
             }
         }
+        // URL
+        $url = filter_input(INPUT_POST, 'url', FILTER_SANITIZE_URL);
+        if(!empty($url)) {
+            $isOk = filter_var($url, FILTER_VALIDATE_URL);
+            if(!$isOk || !str_contains($url, 'linkedin.com/in/')) {
+                $error['url'] = 'Votre url n\'est pas valide';
+            }
+        }
+        // PAYS DE NAISSANCE
+        $countryBirth = filter_input(INPUT_POST, 'countryBirth',FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        if(empty($countryBirth)) {
+            $error['countryBirth'] = 'Veuillez séléctionner un Pays';
+        }elseif (!in_array($countryBirth, ['France', 'Belgique', 'Suisse', 'Luxembourg', 'Allemagne', 'Italie', 'Espagne', 'Portugal'])) {
+            $error['countryBirth'] = 'Ce n\'est pas un pays valide';
+        }
+        // CHECKBOX
+        // DATE
+        // CIVILITE
+        // MOT DE PASSE        
+        // IMAGE DE PROFIL
+        // TEXTAREA
+        
     }
 ?>
 <!DOCTYPE html>
@@ -52,37 +76,66 @@
                 <div class="col-12 firstContainer">
                     <h1 class="text-center h1Title">S'inscrire</h1>
                     <?php if($_SERVER['REQUEST_METHOD'] != 'POST' || !empty($error)) { ?>
-                    <form method="post" action="<?=htmlspecialchars($_SERVER["PHP_SELF"])?>" novalidate>
+                    <form method="POST" novalidate>
                         <div class="row">
                             <div class="col-md-6 left">
                                 <!-- EMAIL -->
                                 <div class="mb-3">
                                     <label for="email" class="form-label">Email address* <span class="text-danger"><?=$error['email'] ?? ''?></span></label>
-                                    <input type="email" name="email" value="<?=$email ?? ''?>" class="form-control" id="email" required placeholder="exemple@gmail.com" maxlength="25">
+                                    <input type="email" 
+                                    name="email" 
+                                    value="<?=$email ?? ''?>" 
+                                    class="form-control" 
+                                    id="email" 
+                                    required 
+                                    placeholder="exemple@gmail.com" 
+                                    maxlength="40" 
+                                    autocomplete="email">
                                 </div>
                                 <!-- MOT DE PASSE -->
                                 <div class="mb-3">
                                     <label for="password" class="form-label">Mot de passe*</label> 
-                                    <input type="password" name="password" class="form-control" id="password" minlength="8" pattern="^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\w\d\s:])([^\s]){8,16}$">
+                                    <input type="password" 
+                                    name="password" 
+                                    class="form-control" 
+                                    id="password" 
+                                    minlength="8" 
+                                    pattern="^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\w\d\s:])([^\s]){8,16}$">
                                 </div>
                                 <!-- CONFIRMER LE MOT DE PASSE -->
                                 <div class="mb-3">
                                     <label for="confirmPassword" class="form-label">Confirmer le mot de passe*</label> 
-                                    <input type="password" name="confirmPassword" class="form-control" id="confirmPassword" minlength="8" pattern="^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\w\d\s:])([^\s]){8,16}$">
+                                    <input type="password" 
+                                    name="confirmPassword" 
+                                    class="form-control" 
+                                    id="confirmPassword" 
+                                    minlength="8" 
+                                    pattern="^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\w\d\s:])([^\s]){8,16}$">
                                 </div>
-                                <!-- GENRE -->
-                                <div class="mb-3">
-                                <label for="gender" class="form-label">Séléctionnez un genre*</label>
-                                <select class="form-select" name="gender" id="gender">
-                                    <option selected disabled>Votre civilité</option>
-                                    <option value="1">Mr</option>
-                                    <option value="2">Mme</option>
-                                </select>
+                                <!-- CIVILITE -->
+                                <div class="">
+                                    <label for="gender" class="form-label" required>Séléctionnez un genre*</label>
                                 </div>
+                                    <div class="form-check form-check-inline">
+                                        <input class="form-check-input" type="radio" name="gender" id="gender">
+                                        <label class="form-check-label" for="genderMr">Mr</label>
+                                    </div>
+                                    <div class="form-check form-check-inline mb-3">
+                                        <input class="form-check-input" type="radio" name="gender" id="gender">
+                                        <label class="form-check-label" for="genderMme">Mme</label>
+                                    </div>
                                 <!-- NOM -->
                                 <div class="mb-3">
                                     <label for="lastname" class="form-label">Nom* <span class="text-danger"><?=$error['lastname'] ?? ''?></span></label>
-                                    <input type="text" name="lastname" class="form-control" value="<?=$lastname ?? ''?>" id="lastname" placeholder="Dupont" required minlength="2" maxlength="50" pattern="^[A-Za-zéèçà \-]{2,50}$">
+                                    <input type="text" 
+                                    name="lastname" 
+                                    class="form-control" 
+                                    value="<?=$lastname ?? ''?>" 
+                                    id="lastname" 
+                                    placeholder="Dupont" 
+                                    required 
+                                    pattern=<?=LAST_NAME?> 
+                                    autocomplete="family-name">
                                 </div>
                                 <!-- ANNEE DE NAISSANCE -->
                                 <div class="mb-3">
@@ -91,17 +144,14 @@
                                 </div>
                                 <div class="mb-3">
                                 <!-- LIEU DE NAISSANCE -->
-                                <label for="countryBirth" class="form-label">Pays de naissance*</label>
-                                    <select class="form-select" name="countryBirth" id="countryBirth">
+                                <label for="countryBirth" class="form-label">Pays de naissance* <span class="text-danger"><?=$error['countryBirth'] ?? ''?></span></label>
+                                    <select class="form-select" 
+                                    name="countryBirth" 
+                                    id="countryBirth"
+                                    required>
                                         <option selected disabled>Votre Pays</option>
-                                        <option value="1">France</option>
-                                        <option value="2">Belgique</option>
-                                        <option value="3">Suisse</option>
-                                        <option value="4">Luxembourg</option>
-                                        <option value="5">Allemagne</option>
-                                        <option value="6">Italie</option>
-                                        <option value="7">Espagne</option>
-                                        <option value="8">Portugal</option>
+                                        <option value="France" <?= (isset($countryBirth) && $countryBirth=='France') ? 'selected' : ''?>>France</option>
+                                        
                                     </select>
                                 </div>
                             </div>
@@ -109,7 +159,16 @@
                                 <!-- CODE POSTAL -->
                                 <div class="mb-3">
                                     <label for="postalCode" class="form-label">Code postal* <span class="text-danger"><?=$error['postalCode'] ?? ''?></span></label>
-                                    <input type="text" name="postalCode" class="form-control" value="<?=$postalCode ?? ''?>" id="postalCode" placeholder="exemple : 80000" required pattern="^[0-9]{5}$">
+                                    <input type="text" 
+                                    name="postalCode" 
+                                    class="form-control" 
+                                    value="<?=$postalCode ?? ''?>" 
+                                    id="postalCode" 
+                                    placeholder="exemple : 80000" 
+                                    required 
+                                    pattern=<?=POSTAL_CODE?>
+                                    autocomplete="postal-code" 
+                                    inputmode="numeric">
                                 </div>
                                 <!-- IMAGE DE PROFIL -->
                                 <div class="mb-3">
@@ -118,8 +177,15 @@
                                 </div>
                                 <!-- LIEN LINKEDIN -->
                                 <div class="mb-3">
-                                    <label for="url" class="form-label">Linkedin</label>
-                                    <input type="url" name="url" class="form-control" id="url" placeholder="https://www.linkedin.com/name" pattern="https://.*">
+                                    <label for="url" class="form-label">Linkedin <span class="text-danger"><?=$error['url'] ?? ''?></span></label>
+                                    <input type="url" 
+                                    name="url" 
+                                    value="<?=$url ?? '' ?>" 
+                                    class="form-control" 
+                                    id="url" 
+                                    placeholder="https://www.linkedin.com" 
+                                    pattern="https://.*" 
+                                    autocomplete="url">
                                 </div>
                                 <!-- LANGAGES CHECKBOX -->
                                 <div class="mb-3 d-flex flex-column">
@@ -127,22 +193,21 @@
                                     <div class="form-check d-flex justify-content-around checkboxDiv">
                                         <label class="form-check-label" for="checkbox">
                                         <input class="form-check-input" name="checkbox" type="checkbox" value="html" id="checkboxHtml">
-                                            HTML/CSS
-                                        </label>
+                                        HTML/CSS</label>
                                         <label class="form-check-label" for="checkboxPhp">
                                         <input class="form-check-input" name="checkbox" type="checkbox" value="php" id="checkboxPhp">
                                             PHP
                                         </label>
-                                        <input class="form-check-input" name="checkbox" type="checkbox" value="javascript" id="checkboxJs">
                                         <label class="form-check-label" for="checkboxJs">
+                                        <input class="form-check-input" name="checkbox" type="checkbox" value="javascript" id="checkboxJs">
                                             Javascript
                                         </label>
-                                        <input class="form-check-input" name="checkbox" type="checkbox" value="python" id="checkboxPython">
                                         <label class="form-check-label" for="checkboxPython">
+                                        <input class="form-check-input" name="checkbox" type="checkbox" value="python" id="checkboxPython">
                                             Python
                                         </label>
-                                        <input class="form-check-input" name="checkbox" type="checkbox" value="others" id="checkboxOthers">
                                         <label class="form-check-label" for="checkboxOthers">
+                                        <input class="form-check-input" name="checkbox" type="checkbox" value="others" id="checkboxOthers">
                                             Autres
                                         </label>
                                     </div>
@@ -159,7 +224,12 @@
                     </form>
                     <?php } else { ?>
                         <div>
-                            <?=$lastname.' '.$email.' '.$postalCode?>
+                            <p class="fw-bold">Nom : <?=$lastname?></p>
+                            <p class="fw-bold">Email : <?=$email?></p>
+                            <p class="fw-bold">Postal Code : <?=$postalCode?></p>
+                            <p class="fw-bold">URL : <?=$url?></p>
+                            <p class="fw-bold">Country Birth : <?=$countryBirth?></p>
+                            <p class="fw-bold">Gender : <?=$gender?></p>
                         </div>
                     <?php } ?>
                 </div>
